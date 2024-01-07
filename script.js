3f1df3fb3cb0249f4a86de2e6f2cef9de0677b45
za-h-ra <zahra.khan1@icloud.com>
2024-01-07 13:40:32 -0500

    add another object to locations array

--- script.js
+++ script.js
@@ -25,6 +25,16 @@ const locations = [
     "button functions": [goStore, goCave, fightDragon],
     text: 'You are in the town square. You see a sign that says "Store".',
   },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  },
 ];
 
 // initialize buttons
