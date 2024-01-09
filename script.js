704d4f6390430a333debcad2467a8a62b3f5e95b
za-h-ra <zahra.khan1@icloud.com>
2024-01-09 14:01:44 -0500

    add conditionals to buyHealth function

--- script.js
+++ script.js
@@ -69,11 +69,22 @@ function goStore() {
   update(locations[1]);
 }
 
-function goCave() {}
+function goCave() {
+  update(locations[2]);
+}
 
 function fightDragon() {}
 
-function buyHealth() {}
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    goldText.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health.";
+  }
+}
 function buyWeapon() {}
 
 function fightSlime() {}
