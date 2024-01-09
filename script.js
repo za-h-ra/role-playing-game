3fc5138dbe3f443e8b4a75e83ab0fa2e724f923c
za-h-ra <zahra.khan1@icloud.com>
2024-01-09 13:34:24 -0500

    set up locations array and call it in the goTown() function

--- script.js
+++ script.js
@@ -44,21 +44,24 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-  button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
 
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
 
-  text.innerText =
-    'You are in the town square. You see a sign that says "Store".';
+  text.innerText = location.text;
 }
 
-function goTown() {}
+function goTown() {
+  update(locations[0]);
+}
 
-function goStore() {}
+function goStore() {
+  update(locations[1]);
+}
 
 function goCave() {}
 
